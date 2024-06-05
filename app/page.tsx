91614e6735aa0fe89912120f5bf681da119a5987
kander927 <ilovesemeninmybum@gmail.com>
2024-06-05 09:20:31 -0600

    adefQEgfWRGHAWR

--- app/page.tsx
+++ app/page.tsx
@@ -20,7 +20,8 @@ const Page = () => {
         <div className="absolute inset-x-20 top-0 bg-gradient-to-r from-transparent via-indigo-500 to-transparent h-px w-3/4" />
         <div className="absolute inset-x-60 top-0 bg-gradient-to-r from-transparent via-sky-500 to-transparent h-[5px] w-1/4 blur-sm" />
         <div className="absolute inset-x-60 top-0 bg-gradient-to-r from-transparent via-sky-500 to-transparent h-px w-1/4" />
- 
+
+        
      
         {/* Core component */}
         <SparklesCore
