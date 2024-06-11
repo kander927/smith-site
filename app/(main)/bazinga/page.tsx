136f9d1a24396e6c0aac8cd40bc9044815bc007b
kander927 <mr.xferguson@gmail.com>
2024-06-11 17:28:16 -0600

    w

--- app/(main)/bazinga/page.tsx
+++ app/(main)/bazinga/page.tsx
@@ -4,10 +4,10 @@ import React from "react";
 
 const Page = () => {
   return (
-    <div className="relative w-screen h-screen bg-slate-900 [mask-image:radial-gradient(transparent,white)]">
-      <div className="absolute inset-0 w-full h-full z-0 pointer-events-none"></div>
-      <div className="relative flex items-center justify-center w-full h-full z-20">
-        <h1 className="text-white z-20">bazinga</h1>
+    <div className="relative w-screen h-screen z-0">
+      <div className="absolute inset-0 z-0 pointer-events-none elative w-screen h-screen bg-slate-900 [mask-image:radial-gradient(transparent,white)]"></div>
+      <div className="relative flex items-center justify-center w-full h-full z-10">
+        <h1 className="red-gradient text-3xl text-white z-10">bazinga</h1>
       </div>
     </div>
   );
